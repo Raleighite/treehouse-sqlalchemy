beab4f762d33742039a4eb206ef525f3c13b2055
Trav <tjbailey10@gmail.com>
2021-11-18 14:56:07 -0500

    database created

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from models import (Base, session,
+                    Book, engine)
+
 # import models
 # main menu - add, search, analysis, exit, view
 # add books to the database
@@ -6,3 +9,7 @@
 # search books
 # data cleaning
 # loop runs program (__main__)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
