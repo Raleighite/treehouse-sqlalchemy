beaab31da09cfe1fec027634d888898ba330d3a0
Trav <tjbailey10@gmail.com>
2021-12-07 15:46:08 -0500

    added the analysis tools, completed course

--- app.py
+++ app.py
@@ -206,7 +206,17 @@ def app():
                 time.sleep(1.5)
         elif choice == '4':
             # Book analysis
-            pass
+            newest_book = session.query(Book).order_by(Book.published_date.desc()).first()
+            oldest_book = session.query(Book).order_by(Book.published_date).first()
+            total_books = session.query(Book).count()
+            python_books = session.query(Book).filter(Book.title.like('%Python%')).count()
+            print(f'''\n***** BOOK ANALYSIS *****
+            \rOldest Book:  {oldest_book}
+            \rNewest Book:  {newest_book}
+            \rTotal Books:  {total_books}
+            \rNumber of Python Books: {python_books}''')
+            input('\n Press enter to return to the main menu')
+
         else:
             print('GOODBYE')
             app_running = False
